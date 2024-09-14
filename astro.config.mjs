d071e92296059cb23d09c9d5d115bc19fd6993d7
David Tierney <daveshouse44@hotmail.com>
2024-09-14 17:58:41 -0500

    project init and templates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import sanity from "@sanity/astro";
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    sanity({
+      projectId: "pes5zq7r",
+      dataset: "production",
+      useCdn: false,
+      apiVersion: "2024-09-14",
+      studioBasePath: "/admin",
+    }),
+    sanity(),
+    react(),
+    tailwind(),
+  ],
+});
